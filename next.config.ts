c127e3bd146bdc80ab9de458b0ec11a41453385d
bytevictor <victorgonzarg@gmail.com>
2025-12-26 01:18:52 +0100

    🚀 fix: deploy routes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+const repoName = "byteTasks";
+
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: isProd ? `/${repoName}` : undefined,
+  assetPrefix: isProd ? `/${repoName}/` : undefined,
   images: {
     unoptimized: true,
   },
